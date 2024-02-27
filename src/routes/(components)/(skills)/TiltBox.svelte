db6c8603b685edd968585aaf3ecbf103cbd9f854
Forthtilliath <forth@live.fr>
2024-02-27 19:01:07 +0100

    feat: remove parent div + fix grid

--- src/routes/(components)/(skills)/TiltBox.svelte
+++ src/routes/(components)/(skills)/TiltBox.svelte
@@ -12,22 +12,20 @@
 	};
 </script>
 
-<div>
-	<Shine>
-		<div class="rounded-lg border-2 bg-black/80 p-4 hover:cursor-pointer" use:tilt>
-			<h2
-				class="scroll-m-20 border-b text-3xl font-semibold tracking-tight transition-colors first:mt-0"
-			>
-				{title}
-			</h2>
-			<div class="grid grid-cols-[repeat(auto-fit,minmax(7rem,1fr))] gap-4">
-				{#each list as { name, icon, color }}
-					<div class="flex h-28 w-28 flex-col items-center justify-center gap-3">
-						<svelte:component this={icon} {...config} {color} />
-						<p class="text-center text-sm text-slate-200">{name}</p>
-					</div>
-				{/each}
-			</div>
+<Shine>
+	<div class="rounded-lg border-2 bg-black/80 p-4 hover:cursor-pointer" use:tilt>
+		<h2
+			class="scroll-m-20 border-b text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+		>
+			{title}
+		</h2>
+		<div class="grid grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-4">
+			{#each list as { name, icon, color }}
+				<div class="flex h-28 w-28 flex-col items-center justify-center gap-3">
+					<svelte:component this={icon} {...config} {color} />
+					<p class="text-center text-sm text-slate-200">{name}</p>
+				</div>
+			{/each}
 		</div>
-	</Shine>
-</div>
+	</div>
+</Shine>
