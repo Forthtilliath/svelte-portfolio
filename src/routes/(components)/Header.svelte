3d4b08b0ecba8e2daf6c6966a87d3221842d2453
Forthtilliath <forth@live.fr>
2024-03-01 17:18:16 +0100

    style: change some styles

--- src/routes/(components)/Header.svelte
+++ src/routes/(components)/Header.svelte
@@ -8,7 +8,7 @@
 	class="fixed top-0 z-20 flex w-full items-center bg-app-black/80 p-4 shadow-sm shadow-white backdrop-blur-sm"
 >
 	<div class="mx-auto flex w-full max-w-5xl items-center justify-between">
-		<a href="#hero" class="text-lg font-semibold text-white transition hover:text-app-blue">
+		<a href="#hero" class="text-xl font-extrabold text-white transition-colors hover:text-app-blue">
 			{$t('header.title')}
 		</a>
 
@@ -27,12 +27,12 @@
 			</li>
 			<li class="font-poppins flex cursor-pointer text-base font-medium transition">
 				{#if $locale === 'en'}
-					<button on:click={() => ($locale = 'fr')}>
+					<button class="hover:scale-125 transition-transform" on:click={() => ($locale = 'fr')}>
 						<Fr size={36} />
 						<span class="sr-only">{$t('header.language', { lang: 'french' })}</span>
 					</button>
 				{:else}
-					<button on:click={() => ($locale = 'en')}>
+					<button class="hover:scale-125 transition-transform" on:click={() => ($locale = 'en')}>
 						<Gb size={36} />
 						<span class="sr-only">{$t('header.language', { lang: 'anglais' })}</span>
 					</button>
