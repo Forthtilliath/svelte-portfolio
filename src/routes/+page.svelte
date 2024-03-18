91532a9a89f7d803660d0a2dfb989b01a0b48f3f
Forthtilliath <forth@live.fr>
2024-03-18 18:21:23 +0100

    feat: add shadcn card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,25 @@
 	import Projects from './(components)/(projects)/projects.svelte';
 	import Contact from './(components)/(contact)/contact.svelte';
 
+	import SectionSeparator from '$lib/components/shared/section-separator.svelte';
+
+	import img1 from '$lib/assets/separators/1.jpg';
+	import img2 from '$lib/assets/separators/2.jpg';
+	import img3 from '$lib/assets/separators/3.jpg';
+	import img4 from '$lib/assets/separators/4.jpg';
+
 	export let data;
 </script>
 
 <Header />
 <main>
 	<Hero />
+	<SectionSeparator img={img1} filter={50} />
 	<About />
+	<SectionSeparator img={img2} filter={50} />
 	<Skills />
+	<SectionSeparator img={img3} filter={50} />
 	<Projects />
+	<SectionSeparator img={img4} filter={70} />
 	<Contact data={data.form} />
 </main>
