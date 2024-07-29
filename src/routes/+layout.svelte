8d88ad12f0d2f01aa3d00196c9b5b612728f08a0
Forthtilliath <forth@live.fr>
2024-07-29 18:18:41 +0200

    refactor: update og balises

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,22 +7,24 @@
 <svelte:head>
 	<title>{$t('meta.title')}</title>
 	<meta name="description" content={$t('meta.description')} />
+	<meta name="creator" content={$t('meta.author')} />
 
 	<meta name="darkreader-lock" />
-
-	<meta property="twitter:card" content="summary" />
-	<meta property="twitter:title" content={$t('meta.title')} />
-	<meta property="twitter:description" content={$t('meta.description')} />
-	<meta property="twitter:image" content="https://vincent-lisita.vercel.app/me.jpg" />
-	<meta property="twitter:url" content="https://vincent-lisita.vercel.app/" />
 
 	<meta property="og:title" content={$t('meta.title')} />
 	<meta property="og:description" content={$t('meta.description')} />
 	<meta property="og:image" content="https://vincent-lisita.vercel.app/me.jpg" />
 	<meta property="og:url" content="https://vincent-lisita.vercel.app/" />
-	<meta property="og:type" content="”siteweb”" />
+	<meta property="og:type" content="website">
 	<meta property="og:locale" content="fr_FR" />
 	<meta property="og:locale:alternate" content="en_GB" />
+
+	<meta property="twitter:card" content="summary" />
+	<meta property="twitter:domain" content="vincent-lisita.vercel.app">
+	<meta property="twitter:title" content={$t('meta.title')} />
+	<meta property="twitter:description" content={$t('meta.description')} />
+	<meta property="twitter:image" content="https://vincent-lisita.vercel.app/me.jpg" />
+	<meta property="twitter:url" content="https://vincent-lisita.vercel.app/" />
 </svelte:head>
 
 <slot />
