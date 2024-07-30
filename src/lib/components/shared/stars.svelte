557fd8dce072a70588e35f1ee6b9100508193316
Forthtilliath <forth@live.fr>
2024-07-30 16:43:33 +0200

    style: simplify stars classes

--- src/lib/components/shared/stars.svelte
+++ src/lib/components/shared/stars.svelte
@@ -1,12 +1,12 @@
 <div class="absolute top-0 -z-10">
-	<div id="stars1" />
-	<div id="stars2" />
-	<div id="stars3" />
+	<div class="stars stars1" />
+	<div class="stars stars2" />
+	<div class="stars stars3" />
 </div>
 
 <style lang="scss">
-  @use 'sass:math';
-  $height: 8000;
+	@use 'sass:math';
+	$height: 8000;
 
 	// pop is number which determines the number of stars from height
 	@function multiple-box-shadow($pop, $height: 2000) {
@@ -21,57 +21,42 @@
 	$shadows-medium: multiple-box-shadow(10, $height);
 	$shadows-big: multiple-box-shadow(20, $height);
 
-	#stars1 {
-		width: 1px;
-		height: 1px;
+	.stars {
+		width: var(--size);
+		height: var(--size);
 		background: transparent;
-		box-shadow: $shadows-small;
-		animation: animStar 50s linear infinite;
+		box-shadow: var(--shadows);
+		animation: animStar var(--duration) linear infinite;
 
 		&:after {
 			content: ' ';
 			position: absolute;
 			top: 100dvh;
-			width: 1px;
-			height: 1px;
+			width: var(--size);
+			height: var(--size);
 			background: transparent;
-			box-shadow: $shadows-small;
+			box-shadow: var(--shadows);
 		}
 	}
-	#stars2 {
-		width: 2px;
-		height: 2px;
-		background: transparent;
-		box-shadow: $shadows-medium;
-		animation: animStar 100s linear infinite;
 
-		&:after {
-			content: ' ';
-			position: absolute;
-			top: 100dvh;
-			width: 2px;
-			height: 2px;
-			background: transparent;
-			box-shadow: $shadows-medium;
-		}
+	.stars1 {
+		--size: 1px;
+		--shadows: #{$shadows-small};
+		--duration: 50s;
 	}
-	#stars3 {
-		width: 3px;
-		height: 3px;
-		background: transparent;
-		box-shadow: $shadows-big;
-		animation: animStar 150s linear infinite;
 
-		&:after {
-			content: ' ';
-			position: absolute;
-			top: 100dvh;
-			width: 3px;
-			height: 3px;
-			background: transparent;
-			box-shadow: $shadows-big;
-		}
+	.stars2 {
+		--size: 2px;
+		--shadows: #{$shadows-medium};
+		--duration: 100s;
 	}
+
+	.stars3 {
+		--size: 3px;
+		--shadows: #{$shadows-big};
+		--duration: 150s;
+	}
+
 	@keyframes animStar {
 		from {
 			transform: translateY(0px);
