7f7b3b94cec4dc2855d4e6537169f18cb7a8a209
Forthtilliath <forth@live.fr>
2024-07-30 05:10:42 +0200

    chore: add prop onChange

--- src/lib/components/shared/pagination.svelte
+++ src/lib/components/shared/pagination.svelte
@@ -1,4 +1,6 @@
 <script lang="ts" generics="T extends Record<string, unknown>">
+	import type { CustomEventHandler } from 'bits-ui';
+
 	import * as Pagination from '$lib/components/ui/pagination';
 	import ChevronLeft from 'svelte-radix/ChevronLeft.svelte';
 	import ChevronRight from 'svelte-radix/ChevronRight.svelte';
@@ -8,6 +10,7 @@
 		perPage: number;
 		siblingCount: number;
 		data: T[];
+		onChange?: (event: CustomEventHandler<MouseEvent, HTMLDivElement>, page: number) => void;
 	};
 
 	type $$Slots = {
@@ -19,44 +22,51 @@
 	export let perPage: $$Props['perPage'];
 	export let siblingCount: $$Props['siblingCount'];
 	export let data: $$Props['data'] = [];
+	export let onChange: Exclude<$$Props['onChange'], undefined> = () => {};
 </script>
 
 <Pagination.Root count={data.length} {perPage} {siblingCount} let:pages let:currentPage>
-	{@const pageData = currentPage
-		? data.slice((currentPage - 1) * perPage, currentPage * perPage)
-		: data}
-	<main class="mb-4 grid w-full grid-cols-1 justify-items-center gap-4 sm:grid-cols-projects">
-		{#each pageData as itemData}
-			<slot name="card" {itemData} />
-		{/each}
-	</main>
-	{#if pages.length > 1}
-		<Pagination.Content>
-			<Pagination.Item>
-				<Pagination.PrevButton>
-					<ChevronLeft class="h-4 w-4" />
-					<span class="hidden sm:block">{$t('ui.previous')}</span>
-				</Pagination.PrevButton>
-			</Pagination.Item>
-			{#each pages as page (page.key)}
-				{#if page.type === 'ellipsis'}
-					<Pagination.Item>
-						<Pagination.Ellipsis />
-					</Pagination.Item>
-				{:else}
-					<Pagination.Item>
-						<Pagination.Link {page} isActive={currentPage === page.value}>
-							{page.value}
-						</Pagination.Link>
-					</Pagination.Item>
-				{/if}
+	{#if currentPage}
+		{@const pageData = currentPage
+			? data.slice((currentPage - 1) * perPage, currentPage * perPage)
+			: data}
+		<main class="mb-4 grid w-full grid-cols-1 justify-items-center gap-4 sm:grid-cols-projects">
+			{#each pageData as itemData}
+				<slot name="card" {itemData} />
 			{/each}
-			<Pagination.Item>
-				<Pagination.NextButton>
-					<span class="hidden sm:block">{$t('ui.next')}</span>
-					<ChevronRight class="h-4 w-4" />
-				</Pagination.NextButton>
-			</Pagination.Item>
-		</Pagination.Content>
+		</main>
+		{#if pages.length > 1}
+			<Pagination.Content>
+				<Pagination.Item>
+					<Pagination.PrevButton on:click={(e) => onChange(e, currentPage - 1)}>
+						<ChevronLeft class="h-4 w-4" />
+						<span class="hidden sm:block">{$t('ui.previous')}</span>
+					</Pagination.PrevButton>
+				</Pagination.Item>
+				{#each pages as page (page.key)}
+					{#if page.type === 'ellipsis'}
+						<Pagination.Item>
+							<Pagination.Ellipsis />
+						</Pagination.Item>
+					{:else}
+						<Pagination.Item>
+							<Pagination.Link
+								{page}
+								isActive={currentPage === page.value}
+								on:click={(e) => onChange(e, page.value)}
+							>
+								{page.value}
+							</Pagination.Link>
+						</Pagination.Item>
+					{/if}
+				{/each}
+				<Pagination.Item>
+					<Pagination.NextButton on:click={(e) => onChange(e, currentPage + 1)}>
+						<span class="hidden sm:block">{$t('ui.next')}</span>
+						<ChevronRight class="h-4 w-4" />
+					</Pagination.NextButton>
+				</Pagination.Item>
+			</Pagination.Content>
+		{/if}
 	{/if}
 </Pagination.Root>
