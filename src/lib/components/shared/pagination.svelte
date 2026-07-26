bcb5a4636dcfb76cb648f17a146b1f209817e9b2
Forthtilliath <forth@live.fr>
2026-07-26 12:19:36 +0200

    refactor: reset pagination page via effect instead of remount

--- src/lib/components/shared/pagination.svelte
+++ src/lib/components/shared/pagination.svelte
@@ -15,52 +15,56 @@
 	let { perPage, siblingCount, data = [], onChange = () => {}, card }: Props = $props();
 
 	let page = $state(1);
+
+	$effect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-unused-expressions -- read `data` only to track it as an $effect dependency
+		data;
+		page = 1;
+	});
 </script>
 
-{#key data}
-	<Pagination.Root count={data.length} {perPage} {siblingCount} bind:page onPageChange={onChange}>
-		{#snippet children({ pages, currentPage })}
-			{#if currentPage}
-				{@const pageData = currentPage
-					? data.slice((currentPage - 1) * perPage, currentPage * perPage)
-					: data}
-				<main
-					class="smd:grid-cols-projects max-xs:m-auto mb-4 grid w-full grid-cols-1 justify-center justify-items-center gap-4"
-				>
-					{#each pageData as itemData}
-						{@render card?.({ itemData })}
+<Pagination.Root count={data.length} {perPage} {siblingCount} bind:page onPageChange={onChange}>
+	{#snippet children({ pages, currentPage })}
+		{#if currentPage}
+			{@const pageData = currentPage
+				? data.slice((currentPage - 1) * perPage, currentPage * perPage)
+				: data}
+			<main
+				class="smd:grid-cols-projects max-xs:m-auto mb-4 grid w-full grid-cols-1 justify-center justify-items-center gap-4"
+			>
+				{#each pageData as itemData, i (i)}
+					{@render card?.({ itemData })}
+				{/each}
+			</main>
+			{#if pages.length > 1}
+				<Pagination.Content>
+					<Pagination.Item>
+						<Pagination.PrevButton>
+							<ChevronLeft class="h-4 w-4" />
+							<span class="hidden sm:block">{$t('ui.previous')}</span>
+						</Pagination.PrevButton>
+					</Pagination.Item>
+					{#each pages as page (page.key)}
+						{#if page.type === 'ellipsis'}
+							<Pagination.Item>
+								<Pagination.Ellipsis />
+							</Pagination.Item>
+						{:else}
+							<Pagination.Item>
+								<Pagination.Link {page} isActive={currentPage === page.value}>
+									{page.value}
+								</Pagination.Link>
+							</Pagination.Item>
+						{/if}
 					{/each}
-				</main>
-				{#if pages.length > 1}
-					<Pagination.Content>
-						<Pagination.Item>
-							<Pagination.PrevButton>
-								<ChevronLeft class="h-4 w-4" />
-								<span class="hidden sm:block">{$t('ui.previous')}</span>
-							</Pagination.PrevButton>
-						</Pagination.Item>
-						{#each pages as page (page.key)}
-							{#if page.type === 'ellipsis'}
-								<Pagination.Item>
-									<Pagination.Ellipsis />
-								</Pagination.Item>
-							{:else}
-								<Pagination.Item>
-									<Pagination.Link {page} isActive={currentPage === page.value}>
-										{page.value}
-									</Pagination.Link>
-								</Pagination.Item>
-							{/if}
-						{/each}
-						<Pagination.Item>
-							<Pagination.NextButton>
-								<span class="hidden sm:block">{$t('ui.next')}</span>
-								<ChevronRight class="h-4 w-4" />
-							</Pagination.NextButton>
-						</Pagination.Item>
-					</Pagination.Content>
-				{/if}
+					<Pagination.Item>
+						<Pagination.NextButton>
+							<span class="hidden sm:block">{$t('ui.next')}</span>
+							<ChevronRight class="h-4 w-4" />
+						</Pagination.NextButton>
+					</Pagination.Item>
+				</Pagination.Content>
 			{/if}
-		{/snippet}
-	</Pagination.Root>
-{/key}
+		{/if}
+	{/snippet}
+</Pagination.Root>
